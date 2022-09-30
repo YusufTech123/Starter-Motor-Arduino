cef141add7ed3d4f3bedcceb6c367a5ce5769e7a
yusuf <yusufghazali.yg@gmail.com>
2022-09-30 15:01:19 +0700

    Starter-Motor-Arduino

---
+++ Starter_Motor_Arduino.ino
@@ -0,0 +1,65 @@
+// YusufTech Electronics All Rights Reserved REV.1
+// Programmer : Yusuf Ghazali
+#include <SoftwareSerial.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h> 
+
+SoftwareSerial modulBluetooth(0,1); 
+LiquidCrystal_I2C lcd (0x27, 16, 2);
+char perintah;
+const int relayStarter = 5;
+const int relayShutdown = 6;
+const int relayRem = 9;
+
+void setup() {
+Serial.begin(115200);
+lcd.init();
+lcd.backlight(); 
+modulBluetooth.begin(9600);
+pinMode(relayStarter, OUTPUT);
+pinMode(relayShutdown, OUTPUT);
+pinMode(relayRem, OUTPUT);
+digitalWrite(relayStarter, HIGH);
+digitalWrite(relayShutdown, HIGH);
+digitalWrite(relayRem, HIGH);
+}
+
+void loop() {
+  if(modulBluetooth.available()) 
+{
+  perintah = modulBluetooth.read();
+  if( perintah == 'A' ) { 
+    digitalWrite(relayShutdown, LOW);
+    delay(2000);
+    lcd.setCursor(0,0);
+    lcd.print("MENGHIDUPKAN");
+    lcd.setCursor(0,1);
+    lcd.print("MOTOR...");
+    digitalWrite(relayRem, LOW);
+    delay(500);
+    digitalWrite(relayStarter, LOW);
+    delay(1000);
+    digitalWrite(relayStarter, HIGH);
+    digitalWrite(relayRem, HIGH);
+    delay(1000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("MOTOR HIDUP");
+    delay(1000);
+    lcd.clear();
+  }
+    if( perintah == 'S' ) {
+      lcd.setCursor(0,0);
+      lcd.print("MEMATIKAN");
+      lcd.setCursor(0,1);
+      lcd.print("MOTOR");
+      delay(1000);
+      digitalWrite(relayShutdown, HIGH);
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("MOTOR MATI");
+      delay(1000);
+      lcd.clear();
+    }
+  }
+}
